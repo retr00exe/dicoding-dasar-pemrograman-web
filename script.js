00ca766d8d7671b6c0be9e093e6a8aa5e953346e
retr00exe <mekelilyasa3@gmail.com>
2020-07-24 23:24:40 +0700

    Final

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 const parallax = document.getElementById('parallax');
 
-window.addEventListener("scroll", function (){
+window.addEventListener("scroll", function(){
     let offset = window.pageYOffset;
     parallax.style.backgroundPositionY = offset * 0.7 + "px";
 });
 
-window.addEventListener("scroll", function (){
+window.addEventListener("scroll", function(){
     let menuArea = document.getElementsByTagName('nav')[0]
-    console.log(window.pageYOffset)
     if(window.pageYOffset>180){
-        menuArea.classList.add('navbar-color')
+        menuArea.classList.add('navbar-color');
     }else{
-        menuArea.classList.remove('navbar-color')
+        menuArea.classList.remove('navbar-color');
     }
 });
+
+function scrollAppear(){
+    var introText = document.querySelector('.intro-text');
+    var introPosition = introText.getBoundingClientRect().top;
+    var screenPosition = window.innerHeight / 1.5;
+
+    if(introPosition<screenPosition){
+        introText.classList.add('text-appear');
+    }
+}
+
+ window.addEventListener('scroll',scrollAppear);
