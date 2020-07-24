217268da683f64f7b4b026d8e54e61d947180948
retr00exe <mekelilyasa3@gmail.com>
2020-07-24 15:07:44 +0700

    New feature

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const parallax = document.getElementById('parallax');
 
 window.addEventListener("scroll", function (){
     let offset = window.pageYOffset;
-    console.log(offset);
     parallax.style.backgroundPositionY = offset * 0.7 + "px";
+});
+
+window.addEventListener("scroll", function (){
+    let menuArea = document.getElementsByTagName('nav')[0]
+    console.log(window.pageYOffset)
+    if(window.pageYOffset>180){
+        menuArea.classList.add('navbar-color')
+    }else{
+        menuArea.classList.remove('navbar-color')
+    }
 });
